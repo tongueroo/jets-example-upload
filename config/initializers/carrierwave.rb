585d4eb8239462c35c73c02b934b7e8d9a85f168
Tung Nguyen <tongueroo@gmail.com>
2018-12-13 23:07:57 +0000

    use s3 backend for carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['CARRIERWAVE_AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['CARRIERWAVE_AWS_SECRET_ACCESS_KEY'],
+    # use_iam_profile:       true,                         # optional, defaults to false
+    region:                ENV['CARRIERWAVE_AWS_REGION'],
+  }
+  config.fog_directory  = ENV['CARRIERWAVE_S3_BUCKET']
+  config.fog_public     = true                                                  # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{1.hour.to_i}" } # optional, defaults to {}
+end
