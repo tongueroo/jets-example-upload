769ac65c46986a5a18060f5aa59f257b9d91124c
Tung Nguyen <tongueroo@gmail.com>
2018-12-14 23:03:44 +0000

    add cleaner job, update index to link to blog

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ This project demonstrates how to upload images or any binary file with Jets and 
     vim .env.development # add CARRIERWAVE_* variables per tutorial guide
     jets deploy
 
+## Important
+
+Remember to update your `.env` files with the `CARRIERWAVE_*` variables. Example:
+
+.env.development:
+
+CARRIERWAVE_AWS_ACCESS_KEY_ID=AKIAI7KI5LIEXAMPLE
+CARRIERWAVE_AWS_SECRET_ACCESS_KEY=XOGuscz2lNyGFo1ChFntigzBp3IMMGdEXAMPLE
+CARRIERWAVE_AWS_REGION=us-west-2
+CARRIERWAVE_S3_BUCKET=demo-uploads-EXAMPLE
+
+You'll need to update `.env.development.remote` before deploying also.
+
+.env.development.remote:
+
+DATABASE_URL=mysql2://user:pass@host.us-west-2.rds.amazonaws.com/upload?pool=5
---
+++ app/jobs/cleaner_job.rb
@@ -0,0 +1,53 @@
+class CleanerJob < ApplicationJob
+  include Jets::AwsServices
+
+  # Deletes all the current DB records and reseed the db
+  rate "1 day"
+  def clean
+    empty_s3_bucket
+    reset_records
+  end
+
+private
+  # Delete all items in the uploads bucket to keep it clean
+  def empty_s3_bucket
+    s3_bucket_name = ENV['CARRIERWAVE_S3_BUCKET']
+    resp = s3.list_objects(bucket: s3_bucket_name)
+    if resp.contents.size > 0
+      # IE: objects = [{key: "objectkey1"}, {key: "objectkey2"}]
+      objects = resp.contents.map { |item| {key: item.key} }
+      s3.delete_objects(
+        bucket: s3_bucket_name,
+        delete: {
+          objects: objects,
+          quiet: false,
+        }
+      )
+      empty_s3_bucket # keep deleting objects until bucket is empty
+    end
+  end
+
+  def reset_records
+    Post.delete_all
+    3.times do |i|
+      create_post(i)
+    end
+  end
+
+  def create_post(i)
+    post = Post.create(id: i+1, title: "My test post #{i+1}")
+    File.open("db/seed/#{data[i]}") do |f|
+      post.photo = f
+    end
+    post.save!
+  end
+
+  def data
+    %w[
+      jets.png
+      jets-mega-mode.png
+      jets-function-properties.png
+    ]
+  end
+
+end
